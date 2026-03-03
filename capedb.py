51358c6460fbb6ab2cbc7657272e5771310d9f7f
Drew Pihera <drew.pihera@gtri.gatech.edu>
2026-03-03 13:27:50 -0500

    added utility script for controlling upgrades and downgrades

---
+++ capedb.py
@@ -0,0 +1,45 @@
+from alembic import command
+from alembic.config import CommandLine, Config
+
+
+def current(config: Config, verbose=False) -> None:
+    """Displays the currrent version of the database with optional verbosity.
+
+    Args:
+        verbose: True for verbose output, else False. Defaults to `False`.
+    """
+    command.current(config, verbose)
+
+
+def upgrade(config: Config, revision: str) -> None:
+    """Upgrades the DB to the specified revision.
+
+    Args:
+        config: a Config instance.
+        revision: the revision to upgrade to.
+    """
+
+    command.upgrade(config, revision)
+
+
+def downgrade(config: Config, revision: str) -> None:
+    """Downgrades the DB to the specified revision.
+
+    Args:
+        config: a Config instance.
+        revision: the revision to downgrade to.
+    """
+
+    command.downgrade(config, revision)
+
+
+def main():
+    cli = CommandLine()
+    cli.register_command(curr)
+    cli.register_command(upg)
+    cli.register_command(downg)
+    cli.main()
+
+
+if __name__ == "__main__":
+    main()
